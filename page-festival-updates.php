2cb57acafef2a519810c691f8f7bce723e0e7db9
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2018-04-23 21:14:48 -0400

    blerg

--- page-festival-updates.php
+++ page-festival-updates.php
@@ -23,7 +23,7 @@
                    
              
                  </div> <? /* */  ?>
-                 <a href="<? the_permalink(); ?>"><h4><? the_title(); ?></h4></a>
+                 <a href="<php the_permalink();?>"><h4><? the_title(); ?></h4></a>
                  <div class="entry-content">
                    <?
                    global $more;    // Declare global $more (before the loop).
